2e44c1472c34326c2cc2efee81c36fcad1aa2231
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2021-09-14 02:23:24 -0500

    Se agrego integracion con sendgird cuando se cambaia el estatus de un partido

--- play/serializers.py
+++ play/serializers.py
@@ -281,6 +281,26 @@ class MatchUpdateModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
         fields = ['organizer','accepted','category']
+    
+    def update(self, instance, validated_data):
+        mails = [instance.field.managers.user.email]
+        if instance.organizer:
+            mails += instance.organizer.email 
+        message = Mail(
+            from_email='playtogether.app.mx@gmail.com',
+            to_emails= mails,
+            subject='Revisa tus partidos',
+            html_content=f'<strong><h1>Tu partido cambio de estatus</h1></strong>')
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
+        return super().update(instance, validated_data)
 
 # !User FieldManager
     # ?User Manager Creation
